8396c6da520bf2d488371e386d3bd040e5c43377
VJurkov <valeriajurkovic@yahoo.com>
2020-06-08 20:30:56 +0200

    options menu, timer, health bar and torturing with mixamo animations, also lights on cars

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Player player = other.gameObject.GetComponent<Player>();
+            player.TakeDamge(1);
+            Destroy(gameObject);
+        }
+       
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public int maxHealth = 30;
+    public int currentHealth;
+
+    public HealthBar healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // kad pokrenemo igru i svaki put kad udarimo po space na tipkovnici uzet ce 20 kao damage
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamge(1);
+        }
+    }
+
+    public void TakeDamge(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+    }
+}
